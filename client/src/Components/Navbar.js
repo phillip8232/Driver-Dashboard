02833f9ca04cfd4693184455055dfdd02b1f4701
Kensuke Shibata <kensukeshibata@gmail.com>
2019-09-03 15:34:30 +1000

    fixed merge conflict

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -6,28 +6,20 @@ const Navbar = () => (
   <div>
     <Menu fixed="top" inverted>
       <Container>
-<<<<<<< HEAD
-        <Menu.Item as='a' header href='https://www.gofar.co' target='_blank'>
-          <Image size='tiny' src={Logo} style={{ marginRight: '1.5em' }} />
-||||||| merged common ancestors
-        <Menu.Item as='a' header>
-          <Image size='mini' src={Logo} style={{ marginRight: '1.5em' }} />
-=======
         <Menu.Item as="a" header>
           <Image size="mini" src={Logo} style={{ marginRight: "1.5em" }} />
->>>>>>> dev
         </Menu.Item>
         <Menu.Item as="a">Home</Menu.Item>
 
-<<<<<<< HEAD
-        <Dropdown item simple text='More'>
-||||||| merged common ancestors
-        <Dropdown item simple text='Dropdown'>
-=======
         <Dropdown item simple text="Dropdown">
->>>>>>> dev
           <Dropdown.Menu>
-            <Dropdown.Item as='a' href="https://www.gofar.co/about-us/" target="_blank">About Us</Dropdown.Item>
+            <Dropdown.Item
+              as="a"
+              href="https://www.gofar.co/about-us/"
+              target="_blank"
+            >
+              About Us
+            </Dropdown.Item>
             <Dropdown.Item>Whatever</Dropdown.Item>
             <Dropdown.Divider />
             <Dropdown.Header>Header Item</Dropdown.Header>
