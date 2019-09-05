0a90bb946c17cbed74afbc31d78580f673ef54c0
Kensuke Shibata <kensukeshibata@gmail.com>
2019-09-05 10:16:31 +1000

    playing with how many cars

--- client/src/Components/Dropdown.js
+++ client/src/Components/Dropdown.js
@@ -3,35 +3,6 @@ import { Dropdown } from "semantic-ui-react";
 import { graphql } from "react-apollo";
 import { getDashboardAllDataQuery } from "../queries/queries";
 
-const carOptions = [
-  {
-    key: "Honda",
-    text: "Honda",
-    value: "Honda"
-  },
-  {
-    key: "Toyota",
-    text: "Toyota",
-    value: "Toyota"
-  },
-  {
-    key: "Mercedez Benz",
-    text: "Mercedez Benz",
-    value: "Mercdez Benz"
-  }
-];
-
-// const VideoList = ({ videos, onVideoSelect }) => {
-
-//   const renderedList = videos.map(video => {
-//     return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />
-//   });
-
-//   return (
-//     <div className="ui relaxed divided list">{renderedList}</div>
-//   )
-// }
-
 class CarDropDown extends Component {
   render() {
     return (
@@ -40,7 +11,23 @@ class CarDropDown extends Component {
         placeholder="Select Car"
         fluid
         selection
-        options={carOptions}
+        options={[
+          {
+            key: "Honda",
+            text: "Honda",
+            value: "Honda"
+          },
+          {
+            key: "Toyota",
+            text: "Toyota",
+            value: "Toyota"
+          },
+          {
+            key: "Mercedez Benz",
+            text: "Mercedez Benz",
+            value: "Mercdez Benz"
+          }
+        ]}
       />
     );
   }
--- client/src/Components/Sample.js
+++ client/src/Components/Sample.js
@@ -2,56 +2,36 @@ import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import { getDashboardAllDataQuery } from "../queries/queries";
 
-// const carOptions = [
-//   {
-//     key: "Honda",
-//     text: "Honda",
-//     value: "Honda"
-//   },
-//   {
-//     key: "Toyota",
-//     text: "Toyota",
-//     value: "Toyota"
-//   },
-//   {
-//     key: "Mercedez Benz",
-//     text: "Mercedez Benz",
-//     value: "Mercdez Benz"
-//   }
-// ];
-
-// const VideoList = ({ videos, onVideoSelect }) => {
-//   const renderedList = videos.map(video => {
-//     return (
-//       <VideoItem
-//         key={video.id.videoId}
-//         onVideoSelect={onVideoSelect}
-//         video={video}
-//       />
-//     );
-//   });
-
-//   return <div className="ui relaxed divided list">{renderedList}</div>;
-// };
-
-{
-  /* <h1>{this.props.data.owner.cars[0].carName}</h1>
-        <h1>{this.props.data.owner.cars[1].carName}</h1>
-        <h1>{this.props.data.owner.cars[2].carName}</h1> */
-}
+const carOptions = [
+  {
+    key: "Honda",
+    text: "Honda",
+    value: "Honda"
+  },
+  {
+    key: "Toyota",
+    text: "Toyota",
+    value: "Toyota"
+  },
+  {
+    key: "Mercedez Benz",
+    text: "Mercedez Benz",
+    value: "Mercdez Benz"
+  }
+];
 
 class Sample extends Component {
   render() {
     const carLists = this.props.data.owner.cars;
     console.log(carLists);
 
-    const ownerCarList = carLists => {
-      return carLists[0].carName;
-    };
+    const ownerCarList = carLists.map(car => {
+      return car.carName;
+    });
 
     return (
       <>
-        <h1>{ownerCarList(carLists)}</h1>
+        <h1>{ownerCarList}</h1>
       </>
     );
   }
