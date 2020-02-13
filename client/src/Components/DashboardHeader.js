177493cc278e8243ea87beca338b5584a390a810
Hayden <hayden.fuller@gmail.com>
2020-02-13 12:39:20 +1100

    added airbnb/eslin/prettier styleguide

--- client/src/Components/DashboardHeader.js
+++ client/src/Components/DashboardHeader.js
@@ -1,30 +1,30 @@
-import React from "react";
-import { Statistic, Header, Divider } from "semantic-ui-react";
+import React from 'react'
+import { Statistic, Header, Divider } from 'semantic-ui-react'
 
 const DashboardHeader = () => {
-  return (
-    <div className="hero-img">
-      <Header as="h1">Welcome back TODO!</Header>
-      <div className="hero-img-car-data">
-        <h3>TODO</h3>
-        <Divider hidden />
-        <Statistic.Group size="mini">
-          <Statistic>
-            <Statistic.Value>TODO</Statistic.Value>
-            <Statistic.Label>Trips </Statistic.Label>
-          </Statistic>
-          <Statistic>
-            <Statistic.Value>TODO KM</Statistic.Value>
-            <Statistic.Label>Total Distance</Statistic.Label>
-          </Statistic>
-          <Statistic>
-            <Statistic.Value>TODO</Statistic.Value>
-            <Statistic.Label>Time in Car (hrs) </Statistic.Label>
-          </Statistic>
-        </Statistic.Group>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className="hero-img">
+            <Header as="h1">Welcome back TODO!</Header>
+            <div className="hero-img-car-data">
+                <h3>TODO</h3>
+                <Divider hidden />
+                <Statistic.Group size="mini">
+                    <Statistic>
+                        <Statistic.Value>TODO</Statistic.Value>
+                        <Statistic.Label>Trips </Statistic.Label>
+                    </Statistic>
+                    <Statistic>
+                        <Statistic.Value>TODO KM</Statistic.Value>
+                        <Statistic.Label>Total Distance</Statistic.Label>
+                    </Statistic>
+                    <Statistic>
+                        <Statistic.Value>TODO</Statistic.Value>
+                        <Statistic.Label>Time in Car (hrs) </Statistic.Label>
+                    </Statistic>
+                </Statistic.Group>
+            </div>
+        </div>
+    )
+}
 
-export default DashboardHeader;
+export default DashboardHeader
--- client/src/Components/DashboardSidebar.js
+++ client/src/Components/DashboardSidebar.js
@@ -1,73 +1,77 @@
-import React, { Component } from "react";
-import CarDropDown from "./Dropdown";
-import { DashboardPage } from "../Pages/Dashboard/DashboardPage";
-import LogoutModal from "./LogoutModal";
-import { Button, Icon, Menu, Segment, Sidebar } from "semantic-ui-react";
+import React, { Component } from 'react'
+import CarDropDown from './Dropdown'
+import { DashboardPage } from '../Pages/Dashboard/DashboardPage'
+import LogoutModal from './LogoutModal'
+import { Button, Icon, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
 export default class DashboardSidebar extends Component {
-  state = {
-    visible: false,
-    icon: "sidebar"
-  };
+    state = {
+        visible: false,
+        icon: 'sidebar',
+    }
 
-  handleHideClick = () => this.setState({ visible: false });
-  handleShowClick = () => this.setState({ visible: true, icon: "" });
-  handleSidebarHide = () => this.setState({ visible: false, icon: "sidebar" });
-  DisplayCarData = () => this.setState({ visible: false });
+    handleHideClick = () => this.setState({ visible: false })
+    handleShowClick = () => this.setState({ visible: true, icon: '' })
+    handleSidebarHide = () => this.setState({ visible: false, icon: 'sidebar' })
+    DisplayCarData = () => this.setState({ visible: false })
 
-  render() {
-    const { visible, icon } = this.state;
-    return (
-      <>
-        <div className="navbar-bg">
-          <Button
-            className="navbar-button"
-            disabled={visible}
-            onClick={this.handleShowClick}
-          >
-            <Icon name={icon} style={{ color: "#fff" }} size="big" />
-          </Button>
-        </div>
-        <Sidebar.Pushable as={Segment}>
-          <Sidebar
-            as={Menu}
-            animation="overlay"
-            icon="labeled"
-            inverted
-            onHide={this.handleSidebarHide}
-            vertical
-            visible={visible}
-            width="thin"
-          >
-            <Menu.Item>Welcome User!</Menu.Item>
-            <Menu.Item>
-              <Icon name="car" size="massive" />
-              Cars
-              <CarDropDown />
-              <Button
-                onClick={this.DisplayCarData}
-                className="ui inverted primary basic button"
-              >
-                Show Data
-              </Button>
-            </Menu.Item>
-            <Menu.Item>
-              <Button
-                onClick={this.handleSidebarHide}
-                className="ui inverted red basic button"
-              >
-                <Icon name="arrow circle left" size="large" />
-                Close Sidebar
-              </Button>
-            </Menu.Item>
-            <LogoutModal hide={this.handleSidebarHide} />
-          </Sidebar>
+    render() {
+        const { visible, icon } = this.state
+        return (
+            <>
+                <div className="navbar-bg">
+                    <Button
+                        className="navbar-button"
+                        disabled={visible}
+                        onClick={this.handleShowClick}
+                    >
+                        <Icon
+                            name={icon}
+                            style={{ color: '#fff' }}
+                            size="big"
+                        />
+                    </Button>
+                </div>
+                <Sidebar.Pushable as={Segment}>
+                    <Sidebar
+                        as={Menu}
+                        animation="overlay"
+                        icon="labeled"
+                        inverted
+                        onHide={this.handleSidebarHide}
+                        vertical
+                        visible={visible}
+                        width="thin"
+                    >
+                        <Menu.Item>Welcome User!</Menu.Item>
+                        <Menu.Item>
+                            <Icon name="car" size="massive" />
+                            Cars
+                            <CarDropDown />
+                            <Button
+                                onClick={this.DisplayCarData}
+                                className="ui inverted primary basic button"
+                            >
+                                Show Data
+                            </Button>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Button
+                                onClick={this.handleSidebarHide}
+                                className="ui inverted red basic button"
+                            >
+                                <Icon name="arrow circle left" size="large" />
+                                Close Sidebar
+                            </Button>
+                        </Menu.Item>
+                        <LogoutModal hide={this.handleSidebarHide} />
+                    </Sidebar>
 
-          <Sidebar.Pusher dimmed={visible}>
-            <DashboardPage {...this.props} />
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </>
-    );
-  }
+                    <Sidebar.Pusher dimmed={visible}>
+                        <DashboardPage {...this.props} />
+                    </Sidebar.Pusher>
+                </Sidebar.Pushable>
+            </>
+        )
+    }
 }
