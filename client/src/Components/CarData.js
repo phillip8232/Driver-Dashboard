ec5f5b650fb65a95a46a125bf27af4164dc0384c
Hayden <hayden.fuller@gmail.com>
2020-02-18 11:46:07 +1100

    removed console.log and added rule to eslint for noconsolelog

--- client/src/Components/CarData.js
+++ client/src/Components/CarData.js
@@ -29,7 +29,6 @@ export default function CarData(props) {
   } else {
     return (
       <div className="ui container">
-        {console.log(data)}
         <Card.Group>
           <LastFillUpCard
             lastFillUp={data.car.lastFillUp}
