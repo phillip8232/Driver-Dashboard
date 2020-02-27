f322bade615ef862f6e16a4b1785be63273a7ce8
Hayden <hayden.fuller@gmail.com>
2020-02-27 16:51:59 +1100

    login first page on load

--- client/.next/server/static/development/pages/dashboard.js
+++ client/.next/server/static/development/pages/dashboard.js
@@ -88,7 +88,7 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -2618,7 +2618,7 @@ const getDashboardAllDataQuery = apollo_boost__WEBPACK_IMPORTED_MODULE_0__["gql"
 
 /***/ }),
 
-/***/ 3:
+/***/ 4:
 /*!**********************************!*\
   !*** multi ./pages/dashboard.js ***!
   \**********************************/
---
+++ client/pages/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import fetch from 'node-fetch';
+import Cookies from 'js-cookie';
+import Cookiez from 'next-cookies';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { Redirect } from 'react-router-dom';
+import LoginView from '../src/pages/Login/LoginView';
+import { useRouter } from 'next/router';
+
+function handleSuccessfulLogin(router) {
+  return function(loginResult) {
+    Cookies.set('GofarDashboardUserId', loginResult.userId, { expires: 7 });
+    Cookies.set('GofarDashboardToken', loginResult.authToken, { expires: 7 });
+    router.push('/dashboard');
+  };
+}
+
+export default function LoginPage(props) {
+  if (props.authToken) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  const router = useRouter();
+
+  const client = new ApolloClient({
+    uri:
+      'https://jt63wdhqqre6rpjo5lnylfhjqm.appsync-api.ap-southeast-2.amazonaws.com/graphql',
+    headers: {
+      Authorization: props.authToken,
+      'X-API-KEY': 'da2-d5trhzrnqraznclr2teue2bmmu',
+    },
+    fetch,
+  });
+  return (
+    <ApolloProvider client={client}>
+      <LoginView
+        {...props}
+        handleSuccessfulLogin={handleSuccessfulLogin(router)}
+      />
+    </ApolloProvider>
+  );
+}
+
+LoginPage.getInitialProps = ctx => {
+  return {
+    cookiez: Cookiez(ctx) || '',
+  };
+};
